28735744f6ffc50a6754d9f0f4a32d78f69c60e9
Shoaib4447 <125224624+Shoaib4447@users.noreply.github.com>
2024-05-26 16:57:26 +0000

    Variable and Constant Lec Completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// Variable , Const Declaration
+
+const accoundId = 123
+let accountEmail = 'shoaib@gmail.com'
+var accoundPassword = 'something123'
+accountCity = 'Lahore' //Variable Can be decalred but not good practice
+let accoundState;
+
+// Const are the type of the variable that or not changeable let's try to change it
+// accoundId = 2  //Not Allowed
+// console.log(accoundId);
+// We will recieve an error assignment to constant variable
+
+accountEmail = 'Email Changed'
+accoundPassword = '12123'
+accountCity = 'City Changed Lahore Islamabad'
+
+console.table([accoundId, accoundPassword, accountEmail, accountCity,accoundState])
+
