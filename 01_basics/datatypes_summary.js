049daf31c5eb2399c6314e91e69582190f8ae403
Shoaib4447 <125224624+Shoaib4447@users.noreply.github.com>
2024-05-29 01:43:09 +0500

    Concept of Stack and Heap memory in JS
    Learned about the concepts of the stack and heap memory in javascript

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -5,7 +5,7 @@
 
 
 * => Primitive { CALL BY VALUE } types (7)
-> Copy of a value 
+> Copy of a value original value will not be changed
 1. String
 2. Number
 3. Boolean
@@ -15,7 +15,7 @@
 7. BigInt (ES2020+)
 
 * => Non Primitive { CALL BY REFERENCE } types (3)
-> Reference of the value can be allocated directly
+> Reference of the value can be allocated directly and value will be directly changeable
 1. array
 2. object
 3. function
@@ -51,20 +51,49 @@ let email2; // Undefined
 const id = Symbol("123"); //unique
 const anotherID = Symbol("123"); // unique not same id as above variable both are unique
 const bigNumber = 12345678901234567890n; // bigInt
-console.log(typeof bigNumber)
+console.log(typeof bigNumber);
 // console.log(id === anotherID); // false
 
+// => Non Primitive => typeof() They all are object: array, object ,function
 
-// => Non Primitive => typeof() They all are object: array, object ,function 
-
-const heros = ['Usman','M Fateh', 'Tepu Sultan'] //object 
-const person = {name: 'Shoaib', age: 22, occupation: 'Software Engineer'} //object
-const add = function(a,b){
-    return a + b;
-} // object function
+const heros = ["Usman", "M Fateh", "Tepu Sultan"]; //object
+const person = { name: "Shoaib", age: 22, occupation: "Software Engineer" }; //object
+const add = function (a, b) {
+  return a + b;
+}; // object function
 
 // => How to check type of any variable or constant : typeof()
 // console.log(typeof heros); // array
 
-
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/*
+    ************************** MEMORY WORKING JS *********************** 
+    => Types : Primitive , Non Primitive
+    => Primitive Types : Number , String , Boolean , Null , Undefined , Symbol , BigInt
+    => Non Primitive Types : Object , Array , Function
+    
+    ? which memory and where ? 
+    * => Stack Memory  (primitive)
+    * => Heap Memory  (non primitive)
+    
+    =>Let's Understand with example
+*/
+
+// Primitive
+let marksVariable = 'I am primitive datatype that why I am using stack memory and you can change value of my copy not my value {call by value}'
+let anotherVariable = marksVariable
+anotherVariable = 'Bhatti you changed the value of the copy not original marks value'
+console.table([marksVariable,anotherVariable])
+
+// Non Primitive
+let user = {
+    name:"I am non primitive datatype that why I am using heap memory and you can change value of my original value not my copy  {call by reference}",
+    age:22,
+    occupation:"Software Engineer"
+}
+let anotherUser = user
+anotherUser.age = 15
+anotherUser.occupation = 'developer'
+anotherUser.name = 'Bhatti you changed the value of the original user value not copy'
+console.table([user,anotherUser])
