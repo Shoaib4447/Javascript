30c6fd8ba6c05e1f1046d7ea01cf813e48cdd45c
Shoaib4447 <125224624+Shoaib4447@users.noreply.github.com>
2024-06-01 12:37:19 +0500

    add 03_objects
    => Two ways to access values from object
    => How to changes values of object
    => How to access string key of object
    => How to access Symbol key of object
    => How to freeze object
    => How to add function in object
    => use of this keyword in object
    => creating object using constructor

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,63 @@
+// We can declare objects like literal and constructor
+
+// Use Symbol as a key in objects
+let mySym = Symbol("key1");
+
+// object literal
+let person = {
+  "Full Name": "Shoaib Bhatti",
+  name: "John",
+  [mySym]: "mykey1",
+  age: 30,
+  email: "john@gmail.com",
+  location: "Lahore",
+};
+
+// How to access object literal keys or values
+
+//=> Two way to access the values of objects
+console.log(person.email);
+console.log(person["email"]); 
+console.log(person["Full Name"]); // ['mySym'] tries to access a property with the string key 
+
+// Lets access the symbol and check its type
+console.log(typeof person['mySym']); 
+// String Datatype but we need Symbol datatype
+console.log(person.mySym); // undefined because mySym is not a string key
+console.log( person[mySym]); // result : value
+console.log(typeof mySym); // Symbol
+// write key in [mySym] then it will be Symbol in object
+
+// Change values of the objects
+person.name = "Shoaib";
+console.log(person.name); // Changed from 'john to Shoaib
+// freeze object
+Object.freeze(person);
+// person.name = "Shoaib"; // Object is freeze no changes will propagate
+
+
+// Adding function to Object
+person.greet = function(){
+    console.log("Hello World");
+};
+
+// Referencing values of the same object in function
+person.greet2 = function(){
+    console.log(`Salam ${this.name}`);
+};
+
+console.log(person.greet2());
+
+
+
+
+// constructor object || singleton object
+let person2 = new Object();
+person2.name = "Jane";
+person2.age = 25;
+
+// Creating another Object using Object.create
+let person3 = Object.create(person);
+person3.name = 'Bhatti'
+person3.age = 30;
+console.log(person3); // it will inherit all properties of person object
