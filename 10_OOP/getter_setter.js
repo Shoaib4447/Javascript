080223ad55f08fcfb170adfe300534bb83c63d95
Shoaib Bhatti <shoaiburrehman4447@gmail.com>
2024-07-13 16:11:22 +0500

    Lexical Scope & Closure

--- 10_OOP/getter_setter.js
+++ 10_OOP/getter_setter.js
@@ -3,6 +3,17 @@
 // 1. Getter function is used to get the value of a property.
 // 2. Setter function is used to set the value of a property.
 
+
+// Why
+// Getter and setter functions, also known as accessors, are used to control access to an object's properties. Here are some reasons why we use getter and setter functions:
+// 1.Encapsulation
+// 2.Validation and Sanitization
+// 3.Computed Properties
+// 4.Read-only Properties
+// 5.Improved Code Organization
+// 6.Flexibility and Extensibility
+
+
 class User {
   constructor(email, passward) {
     this.email = email;
